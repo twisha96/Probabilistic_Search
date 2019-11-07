2857a1552af0504bdc9fca24e6e877c5008e9b01
Twisha Naik <twishanaik@Twishas-MacBook-Pro.local>
2019-11-07 01:19:45 -0500

    updating files

--- cell.py
+++ cell.py
@@ -1,15 +1,16 @@
 class Cell:
 
-	def __init__(cell, terrain_type):
+	def __init__(cell):
 		"""
 		type = 0 => cell is flat
  		type = 1 => cell is hilly
 		type = 2 => cell is forested
 		type = 3 => cell is a maze of caves
 		"""
-		cell.type = terrain_type
-		cell.p_target = 0
+		cell.type = -1
+		cell.p_target = -1
 		cell.visited = False
+		cell.is_target = False
 		false_negative = {
 			0: 0.1,
 			1: 0.3,
--- game_board.py
+++ game_board.py
@@ -6,49 +6,8 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
 
-def assign_hidden_squares_to_edge_cells(board, dim):
-	if dim > 1:
-		for index in xrange(1, dim-1):
-			cell = board[index][0]
-			cell.hidden_squares = 5
-			cell.no_of_neigbors = 5
-			cell = board[index][dim-1]
-			cell.hidden_squares = 5
-			cell.no_of_neigbors = 5
-			cell = board[0][index]
-			cell.hidden_squares = 5
-			cell.no_of_neigbors = 5
-			cell = board[dim-1][index]
-			cell.hidden_squares = 5
-			cell.no_of_neigbors = 5
 
-def assign_hidden_squares_to_corner_cells(board, dim):
-	cell = board[0][0]
-	cell.hidden_squares = 3
-	cell.no_of_neigbors = 3
-	cell = board[0][dim-1]
-	cell.hidden_squares = 3
-	cell.no_of_neigbors = 3
-	cell = board[dim-1][0]
-	cell.hidden_squares = 3
-	cell.no_of_neigbors = 3
-	cell = board[dim-1][dim-1]
-	cell.hidden_squares = 3
-	cell.no_of_neigbors = 3
-
-def get_knowledge_base_open_safe_cells(board, dim):
-	print "Knowlege Base: "
-	for i in range(dim):
-		for j in range(dim):
-			cell = board[i][j]
-			if cell.value==0 and (cell.set_value!=0 or (cell.set_value==0 and len(cell.set_of_unknown_neighbors)!=0)):
-				print i,j," set: ", sorted(cell.set_of_unknown_neighbors), cell.set_value
-
-def get_knowledge_base(board, dim, set_of_all, set_value):
-	get_knowledge_base_open_safe_cells(board, dim)
-	print "Board knowledge: ", sorted(set_of_all), set_value
-
-# returns a list of 8 neighbors around a given cell
+# returns a list of 4 neighbors around a given cell
 def get_neighbors(board, x, y):
 	dim = len(board)
 	neighbor_coordinates = []
@@ -56,60 +15,34 @@ def get_neighbors(board, x, y):
 		neighbor_coordinates.append((x, y-1))
 	if(y+1<=dim-1):
 		neighbor_coordinates.append((x, y+1))	
-
 	if(x-1>=0):
 		neighbor_coordinates.append((x-1, y))
-		if(y-1>=0):
-			neighbor_coordinates.append((x-1, y-1))
-		if(y+1<=dim-1):
-			neighbor_coordinates.append((x-1, y+1))
-
 	if(x+1<=dim-1):
 		neighbor_coordinates.append((x+1, y))
-		if(y-1>=0):
-			neighbor_coordinates.append((x+1, y-1))
-		if(y+1<=dim-1):
-			neighbor_coordinates.append((x+1, y+1))
 
 	return neighbor_coordinates
 
-# returns board with dimension dim with n mines
-def get_board(dim, n):
-	board = [[Cell() for i in range(dim)] for j in range(dim)]
-	
-	# this is return a list of n numbers that are locations of mines
-	# indexed in row-wise manner
-	mine_index = random.sample(range(dim*dim), n)
-	# print mine_index
-	# get the coordinates x and y using the position index
-	mine_locations = []
-	for mine_location in mine_index:
-		row = mine_location/dim
-		col = mine_location%dim
-		board[row][col].is_mine = True
-		mine_locations.append((row, col))
-	
-	# get the neighbors of the mine cells and update the clue value
-	for mine_location in mine_locations:
-		row = mine_location[0]
-		col = mine_location[1]
-		neighbors = get_neighbors(board, row, col)
-		# print row, col, neighbors
-		for neighbor in neighbors:
-			neighbor_cell = board[neighbor[0]][neighbor[1]]
-			if not neighbor_cell.is_mine:
-				neighbor_cell.clue += 1
+# returns cell map with dimension dim with n mines
+def get_cell_map(dim, prob_list):
+	cell_map = [[Cell() for i in range(dim)] for j in range(dim)]
+	cumulative_prob_list = []
 
-	for row_index in range(0, dim):
-		for col_index in range(0, dim):
-			cell = board[row_index][col_index]
-			neighbors = get_neighbors(board, row_index, col_index)
-			cell.set_of_unknown_neighbors = set(neighbors)
-
-	assign_hidden_squares_to_corner_cells(board, dim)
-	assign_hidden_squares_to_edge_cells(board, dim)
-	
-	return board
+	# compute cumulative probability list
+	cumulative_prob_list[0] = prob_list[0]
+	for i in range(len(prob_list)-1):
+		cumulative_prob_list[i+1] = cumulative_prob_list[i] + prob_list[i+1]
+						
+	# assign terrain type to each cell in the map
+	for row in range(dim):
+		for col in range(dim):
+			random_num = random.uniform(0, 1)
+			for terrain_type, prob in enumerate(cumulative_prob_list):
+				if random_num<prob:
+					cell_map[row][col].type = terrain_type
+	target_location = random.randint(0, dim*dim)
+	cell_map[target_location/dim][target_location%dim].is_target = True
+	 
+	return cell_map
 
 
 # visualises the underlying game board generated
