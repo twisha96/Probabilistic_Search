1ceaba41f6a85da7440171fb114a09541c31733a
haripriya2703 <haripriyaponnekanti@gmail.com>
2019-11-11 22:46:06 -0500

    bug fixes

--- rules_analysis.py
+++ rules_analysis.py
@@ -7,23 +7,24 @@ import pandas as pd
 
 import search_and_destroy as sd
 
-dim = 3
+dim = 50
 # fig, ax = plt.subplots()
 # y_rule1 = []
 # y_rule2 = []
 # legend = []
-column_name_list = ['search steps 1', 'exec time 1', 'search steps 2', 'exec time 2', 'search steps 3', 'exec time 3']
+column_name_list = ['search steps 1', 'search steps 2', 'search steps 3', 'exec time 1', 'exec time 2', 'exec time 3']
 prob_list = [0.2, 0.3, 0.3, 0.2]
 analysis_df = pd.DataFrame(columns=column_name_list)
-iterations = 100
+iterations = 10
 terrain_type_list = [0, 1, 2, 3]
 # for terrain_type in terrain_type_list:
 cell_map = cm.get_cell_map(dim, prob_list)
 for j in range(dim):
     for k in range(dim):
         cell_map[j][k].is_target = True
+        terrain_type = cell_map[j][k].type
         # x, y, type = cm.add_target(cell_map)
-        print "x", j, "y", k, "type", cell_map[j][k].type
+        print "x", j, "y", k, "type", terrain_type
         # cm.visualize_board(cell_map)
         for i in range(iterations):
             print "iteration", i
@@ -33,14 +34,13 @@ for j in range(dim):
             print "Rule 2- steps: ", search_steps_2, ", exec time: ", exec_time_2
             search_steps_3, observations_t_3, exec_time_3 = sd.search_cell_map(copy.deepcopy(cell_map), [], 2)
             print "Rule 3- steps: ", search_steps_3, ", exec time: ", exec_time_3
-
-            df_entry = pd.DataFrame([(search_steps_1, exec_time_1, search_steps_2, exec_time_2, search_steps_3, exec_time_3)],
-                                    columns=column_name_list, index=[type])
+            df_entry = pd.DataFrame([(search_steps_1, search_steps_2, search_steps_3, exec_time_1,  exec_time_2,
+                                      exec_time_3)], columns=column_name_list, index=[terrain_type])
             # print "df entry", df_entry
             analysis_df = analysis_df.append(df_entry)
             # y_rule1.append(search_steps_1)
             # y_rule2.append(search_steps_2)
-            analysis_df.to_csv("analysis.csv")
+            analysis_df.to_csv("analysis_6.csv")
         cm.remove_target(cell_map, j, k)
 #
 # ax.plot(density_array, y_baseline)
