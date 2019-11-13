83a60a3be76a17aa94c45cc75b8d2d450d88e3c8
haripriya2703 <haripriyaponnekanti@gmail.com>
2019-11-12 20:56:03 -0500

    replaced p_target with belief

--- search_and_destroy.py
+++ search_and_destroy.py
@@ -16,22 +16,23 @@ def update_belief_old(cell_map, new_cell, rule_no):
         for col in range(0, dim):
             cell = cell_map[row][col]
             if row == new_cell[0] and col == new_cell[1]:
-                updated_belief = float(explored_cell.p_target*explored_cell.false_negative)/float(explored_cell.p_target*explored_cell.false_negative + 1 - explored_cell.p_target)
-                explored_cell.p_target = updated_belief
+                updated_belief = float(explored_cell.belief*explored_cell.false_negative) / \
+                                 float(explored_cell.belief*explored_cell.false_negative + 1 - explored_cell.belief)
+                explored_cell.belief = updated_belief
 
             else:
                 explored_cell_not_found_prob = get_not_found_prob(cell_map, new_cell) # not fj
                 conditional_not_found_prob = float(explored_cell_not_found_prob - cell.initial_probability)/\
                                              float(1-cell.initial_probability)
-                updated_belief = cell.p_target/(cell.p_target+(1-cell.p_target)*conditional_not_found_prob)
-                cell.p_target = updated_belief
+                updated_belief = cell.belief/(cell.belief+(1-cell.belief)*conditional_not_found_prob)
+                cell.belief = updated_belief
 
             if rule_no == 1:
-                if max_belief < cell.p_target*(1-cell.false_negative):
-                    max_belief = cell.p_target*(1-cell.false_negative)
+                if max_belief < cell.belief*(1-cell.false_negative):
+                    max_belief = cell.belief*(1-cell.false_negative)
             else:
-                if max_belief < cell.p_target:
-                    max_belief = cell.p_target
+                if max_belief < cell.belief:
+                    max_belief = cell.belief
 
     # prob_map, sum_prob = cm.probability_sanity_check(cell_map)
     # print "prob_map", prob_map
@@ -42,10 +43,10 @@ def update_belief_old(cell_map, new_cell, rule_no):
         for col in range(0, dim):
             cell = cell_map[row][col]
             if rule_no == 1:
-                if max_belief == cell.p_target*(1-cell.false_negative):
+                if max_belief == cell.belief*(1-cell.false_negative):
                     max_belief_pool.append((row, col))
             else:
-                if max_belief == cell.p_target:
+                if max_belief == cell.belief:
                     max_belief_pool.append((row, col))
 
     return max_belief_pool
@@ -61,26 +62,26 @@ def update_belief(cell_map, new_cell, rule_no):
         for col in range(0, dim):
             cell = cell_map[row][col]
             if row == new_cell[0] and col == new_cell[1]:
-                denominator += cell.p_target * cell.false_negative
+                denominator += cell.belief * cell.false_negative
             else:
-                denominator += cell.p_target
+                denominator += cell.belief
 
     for row in range(0, dim):
         for col in range(0, dim):
             cell = cell_map[row][col]
             if row == new_cell[0] and col == new_cell[1]:
-                updated_belief = float(cell.p_target * cell.false_negative)/denominator
-                cell.p_target = updated_belief
+                updated_belief = float(cell.belief * cell.false_negative)/denominator
+                cell.belief = updated_belief
             else:
-                updated_belief = float(cell.p_target)/denominator
-                cell.p_target = updated_belief
+                updated_belief = float(cell.belief)/denominator
+                cell.belief = updated_belief
 
             if rule_no == 1:
-                if max_belief < cell.p_target*(1-cell.false_negative):
-                    max_belief = cell.p_target*(1-cell.false_negative)
+                if max_belief < cell.belief*(1-cell.false_negative):
+                    max_belief = cell.belief*(1-cell.false_negative)
             else:
-                if max_belief < cell.p_target:
-                    max_belief = cell.p_target
+                if max_belief < cell.belief:
+                    max_belief = cell.belief
 
     # prob_map, sum_prob = cm.probability_sanity_check(cell_map)
     # print "prob_map", prob_map
@@ -91,10 +92,10 @@ def update_belief(cell_map, new_cell, rule_no):
         for col in range(0, dim):
             cell = cell_map[row][col]
             if rule_no == 1:
-                if max_belief == cell.p_target*(1-cell.false_negative):
+                if max_belief == cell.belief*(1-cell.false_negative):
                     max_belief_pool.append((row, col))
             else:
-                if max_belief == cell.p_target:
+                if max_belief == cell.belief:
                     max_belief_pool.append((row, col))
 
     return max_belief_pool
@@ -145,7 +146,7 @@ def search_cell_map(cell_map, observations_t, rule_no):
             #     random_cell_index = random.randint(0, n-1)
             # random_cell = max_belief_pool[random_cell_index]
             random_cell = max_belief_pool[0]
-        observations_t.append((random_cell, cell_map[random_cell[0]][random_cell[1]].p_target,
+        observations_t.append((random_cell, cell_map[random_cell[0]][random_cell[1]].belief,
                                cell_map[random_cell[0]][random_cell[1]].type))
         target_found = query_cell(cell_map, random_cell)
         if target_found:
@@ -155,18 +156,18 @@ def search_cell_map(cell_map, observations_t, rule_no):
 
 
 # Test code
-prob_list = [0.2, 0.3, 0.3, 0.2]
-dim = 3
-observations_t = []
-
-# cell_map, target_cord_x, target_cord_y, terrain_type = cm.get_cell_map(dim, prob_list)
-cell_map = cm.get_cell_map(dim, prob_list)
-target_cord_x, target_cord_y, terrain_type = cm.add_target(cell_map)
-print "Target location:", target_cord_x, target_cord_y
-print "Target terrain type:", terrain_type
-
-# gb.visualize_board(cell_map)
-start_time = time.time()
-search_steps, observations_t, exec_time = search_cell_map(cell_map, observations_t, 0)
-print search_steps
+# prob_list = [0.2, 0.3, 0.3, 0.2]
+# dim = 3
+# observations_t = []
+#
+# # cell_map, target_cord_x, target_cord_y, terrain_type = cm.get_cell_map(dim, prob_list)
+# cell_map = cm.get_cell_map(dim, prob_list)
+# target_cord_x, target_cord_y, terrain_type = cm.add_target(cell_map)
+# print "Target location:", target_cord_x, target_cord_y
+# print "Target terrain type:", terrain_type
+#
+# # gb.visualize_board(cell_map)
+# start_time = time.time()
+# search_steps, observations_t, exec_time = search_cell_map(cell_map, observations_t, 0)
+# print search_steps
 
